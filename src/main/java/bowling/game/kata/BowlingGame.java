06fdb8f2d3b03392cde555d1fdfe8323429ea04a
Bryce Klinker <klinker.bryce@principal.com>
2020-04-29 13:44:38 -0500

    Removed spare calculation. The change is not easy yet

--- src/main/java/bowling/game/kata/BowlingGame.java
+++ src/main/java/bowling/game/kata/BowlingGame.java
@@ -12,11 +12,7 @@ public class BowlingGame {
     public int score() {
         int score = 0;
         for (int i = 0; i < 21; i++) {
-            if (rolls[i] + rolls[i + 1] == 10) {
-                score += 10 + rolls[i + 2];
-            } else {
-                score += rolls[i];
-            }
+            score += rolls[i];
         }
         return score;
     }
