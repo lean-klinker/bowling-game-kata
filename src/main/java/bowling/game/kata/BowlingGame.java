02b769f3a5b3debcc8323827f0a30e7eb714d552
Bryce Klinker <klinker.bryce@principal.com>
2020-04-29 13:31:44 -0500

    Fixing second test with simplest change

--- src/main/java/bowling/game/kata/BowlingGame.java
+++ src/main/java/bowling/game/kata/BowlingGame.java
@@ -1,11 +1,13 @@
 package bowling.game.kata;
 
 public class BowlingGame {
-    public void roll(int pins) {
+    private int score;
 
+    public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
