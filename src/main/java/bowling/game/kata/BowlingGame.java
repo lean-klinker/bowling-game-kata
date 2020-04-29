b8f6eaaa9775057cf14eddc09186fd34d05b63db
Bryce Klinker <klinker.bryce@principal.com>
2020-04-29 13:39:24 -0500

    Refactored scoring to be calculated in score method. 'First make the change easy, then make the easy change' (Kent Beck)

--- src/main/java/bowling/game/kata/BowlingGame.java
+++ src/main/java/bowling/game/kata/BowlingGame.java
@@ -1,13 +1,19 @@
 package bowling.game.kata;
 
 public class BowlingGame {
-    private int score;
+    private int[] rolls = new int[21];
+    private int currentRoll;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll] = pins;
+        currentRoll++;
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < 21; i++) {
+            score += rolls[i];
+        }
         return score;
     }
 }
