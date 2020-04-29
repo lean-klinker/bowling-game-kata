adba1cf3552056c0c8ad58bd1a7a9f9ac7f86d60
Bryce Klinker <klinker.bryce@principal.com>
2020-04-29 13:20:01 -0500

    Added first failing test

--- src/test/java/bowling/game/kata/BowlingGameTest.java
+++ src/test/java/bowling/game/kata/BowlingGameTest.java
@@ -6,8 +6,11 @@ import static org.junit.Assert.*;
 
 public class BowlingGameTest {
     @Test
-    public void testSomething() {
+    public void whenRollingAllGutterBallsThenScoreIsZero() {
         BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
         assertEquals(0, game.score());
     }
 }
